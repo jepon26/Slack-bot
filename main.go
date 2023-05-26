1045783c65e13adc795127b98417d53b3b58b60e
Elvin Javier Rueda <elvinrueda@yahoo.es>
2023-05-25 20:09:34 -0600

    update

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"logo"
 	"os"
 
@@ -28,4 +29,22 @@ func main() {
 	bot := slacker.NewClient(os.Getenv("SLAcK_BOT_TOCKEN"), os.Getenv("SLACK_APP_TOKEN"))
 
 	go printCommandEvents(bot.CommandEvents())
+
+
+	bot.Comand("My age is <year>", &slacker.CommandDefinition){
+		Description := "age calculator",
+		Example := "My age is 1900",
+		Handler: func (botCtx slacker.botContext, request slacker.Request, response slacker.Response)  {
+			
+		}
+	}
+
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err := bot.listen(ctx)
+	if err != nil{
+		log.Fatal(err)
+	}
 }
